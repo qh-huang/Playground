7599c2275f65446943524701218aa6efe6e4878a
qiao <qiao.helloworld@gmail.com>
2019-12-16 10:38:25 +0800

    Add some better practice for modern c++

--- cpp/app_main.cc
+++ cpp/app_main.cc
@@ -1,4 +1,6 @@
+#include <algorithm>
 #include <iostream>
+#include <memory>
 
 #define DISPLAY_FUNC_NAME               \
   do {                                  \
@@ -18,9 +20,26 @@ public:
 };
 
 int main(int argc, char** argv) {
-  {
+  // bad idea: avoid using raw pointer
+  { 
     Derive* p = new Derive();
     delete(p);
   }
+  // good idea: use unique_ptr/shared_ptr instead
+  {
+    // std::unique_ptr<Derive> p = std::make_unique<Derive>(); // C++ 14
+    std::unique_ptr<Derive> p = std::unique_ptr<Derive>(new Derive()); // C++ 11
+  }
+  
+  // replace old-style array and looping
+  {
+    // instead of int arr[10] = {...};
+    std::array<int, 10> arr {1, 1, 2, 3, 5, 8, 13, 21, 34, 55};
+    
+    // instead of for (int i=0; i<arr.size(); i++) { ... }
+    std::for_each(arr.begin(), arr.end(), [&](const int i) -> void { std::cout << i << ' '; });
+    std::cout << '\n'; // std::endl implies a std::flush, only use it when needed
+  }
+
   return 0;
 }
