b03536888e80f2f93916dbade22cc032a7fc36c6
qiao <qiao.helloworld@gmail.com>
2020-01-07 00:23:37 +0800

    docker: Fix run break on Linux

--- docker/run_container.sh
+++ docker/run_container.sh
@@ -10,38 +10,108 @@ set -o xtrace
 # Make sure processes in the container can connect to the x server
 # Necessary so gazebo can create a context for OpenGL rendering (even headless)
 
-XAUTH=/tmp/.docker.xauth
-if [ -d $XAUTH ]
-then
-    sudo rm -rf $XAUTH
-fi
+function init_x11() {
+  XAUTH=/tmp/.docker.xauth
+  if [ -d $XAUTH ]
+  then
+      sudo rm -rf $XAUTH
+  fi
 
-if [ ! -f $XAUTH ]
-then
-    touch $XAUTH
+  if [ ! -f $XAUTH ]
+  then
+      touch $XAUTH
+      xauth_list=$(xauth nlist :0 | head -n 1 | sed -e 's/^..../ffff/')
+      if [ ! -z "$xauth_list" ]
+      then
+          echo $xauth_list | xauth -f $XAUTH nmerge -
+      fi
+      chmod a+r $XAUTH
+  fi
+
+  XAUTH=/tmp/.docker.xauth
+  if [ ! -f $XAUTH ]
+  then
     xauth_list=$(xauth nlist :0 | head -n 1 | sed -e 's/^..../ffff/')
     if [ ! -z "$xauth_list" ]
     then
-        echo $xauth_list | xauth -f $XAUTH nmerge -
+      echo $xauth_list | xauth -f $XAUTH nmerge -
+    else
+      touch $XAUTH
     fi
     chmod a+r $XAUTH
-fi
-
-XAUTH=/tmp/.docker.xauth
-if [ ! -f $XAUTH ]
-then
-  xauth_list=$(xauth nlist :0 | head -n 1 | sed -e 's/^..../ffff/')
-  if [ ! -z "$xauth_list" ]
-  then
-    echo $xauth_list | xauth -f $XAUTH nmerge -
-  else
-    touch $XAUTH
   fi
-  chmod a+r $XAUTH
-fi
 
-# socat TCP-LISTEN:6000,reuseaddr,fork UNIX-CLIENT:\"$DISPLAY\" &
-xhost +
+  # socat TCP-LISTEN:6000,reuseaddr,fork UNIX-CLIENT:\"$DISPLAY\" &
+  xhost +
+}
+
+function prepare_docker_run_params() {
+  case "$(uname -s)" in
+    Darwin)
+      DOCKER_RUN_PARAMETER_LIST="-it \
+        --name=$CONTAINER_NAME \
+        --tmpfs /tmp:exec \
+        -h docker \
+        -e DISPLAY=host.docker.internal:0 \
+        -e QT_X11_NO_MITSHM=1 \
+        -e XAUTHORITY=$XAUTH \
+        -v /dataset:/dataset \
+        -v /etc/group:/etc/group:ro \
+        -v /private/etc/passwd:/etc/passwd:ro \
+        -v /etc/shadow:/etc/shadow:ro \
+        -v /private/etc/sudoers.d:/etc/sudoers.d:ro \
+        -v ${HOME}:/root \
+        -v /tmp/.X11-unix:/tmp/.X11-unix \
+        -v $XAUTH:$XAUTH \
+        -v /dev:/dev \
+        --net=host \
+        --privileged \
+        --detach \
+        --rm ${IMAGE_NAME} \
+        /bin/bash"
+        #-e XAUTHORITY=$XAUTH \
+        #-v "$WORKSPACE_DIR":/home/gpal \
+        #-v /etc/localtime:/etc/localtime:ro \
+        #-v /run/udev:/run/udev:ro \
+      ;;
+
+    Linux)
+      DOCKER_RUN_PARAMETER_LIST="-it \
+        --name=$CONTAINER_NAME \
+        --tmpfs /tmp:exec \
+        -h docker \
+        -e DISPLAY=host.docker.internal:0 \
+        -e QT_X11_NO_MITSHM=1 \
+        -e XAUTHORITY=$XAUTH \
+        -v /dataset:/dataset \
+        -v /etc/group:/etc/group:ro \
+        -v /etc/passwd:/etc/passwd:ro \
+        -v /etc/shadow:/etc/shadow:ro \
+        -v /etc/sudoers.d:/etc/sudoers.d:ro \
+        -v ${HOME}:/root \
+        -v /tmp/.X11-unix:/tmp/.X11-unix \
+        -v $XAUTH:$XAUTH \
+        -v /dev:/dev \
+        --net=host \
+        --privileged \
+        --detach \
+        --rm ${IMAGE_NAME} \
+        /bin/bash"
+        #-e XAUTHORITY=$XAUTH \
+        #-v "$WORKSPACE_DIR":/home/gpal \
+        #-v /etc/localtime:/etc/localtime:ro \
+        #-v /run/udev:/run/udev:ro \
+      ;;
+   CYGWIN*|MINGW32*|MSYS*)
+     echo 'Currently not support MS Windows'
+     ;;
+
+   *)
+     echo 'Unknown OS'
+     ;;
+esac
+}
+
 
 IMAGE_NAME=playground-env
 CONTAINER_NAME=playground-env
@@ -50,31 +120,7 @@ then
   docker start $CONTAINER_NAME
   docker attach $CONTAINER_NAME
 else
-  DOCKER_RUN_PARAMETER_LIST="-it \
-    --name=$CONTAINER_NAME \
-    --tmpfs /tmp:exec \
-    -h docker \
-    -e DISPLAY=host.docker.internal:0 \
-    -e QT_X11_NO_MITSHM=1 \
-    -e XAUTHORITY=$XAUTH \
-    -v /dataset:/dataset \
-    -v /etc/group:/etc/group:ro \
-    -v /private/etc/passwd:/etc/passwd:ro \
-    -v /etc/shadow:/etc/shadow:ro \
-    -v /private/etc/sudoers.d:/etc/sudoers.d:ro \
-    -v ${HOME}:/root \
-    -v /tmp/.X11-unix:/tmp/.X11-unix \
-    -v $XAUTH:$XAUTH \
-    -v /dev:/dev \
-    --net=host \
-    --privileged \
-    --detach \
-    --rm ${IMAGE_NAME} \
-    /bin/bash"
-    #-e XAUTHORITY=$XAUTH \
-    #-v "$WORKSPACE_DIR":/home/gpal \
-    #-v /etc/localtime:/etc/localtime:ro \
-    #-v /run/udev:/run/udev:ro \
+  prepare_docker_run_params
   echo $DOCKER_RUN_PARAMETER_LIST
   docker run $DOCKER_RUN_PARAMETER_LIST
   docker exec -it $CONTAINER_NAME /bin/bash
