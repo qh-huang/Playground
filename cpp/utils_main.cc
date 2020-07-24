78a2ebda8c955ceaf90b71921df1c036bba00a4d
qiao <qiao.helloworld@gmail.com>
2020-07-24 12:19:27 +0800

    Add more utility functions

--- cpp/utils_main.cc
+++ cpp/utils_main.cc
@@ -1,13 +1,47 @@
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
 #include <algorithm>
+#include <array>
 #include <chrono>
 #include <cmath>
+#include <cstdio>
 #include <fstream>
 #include <iostream>
 #include <iterator>
+#include <memory>
 #include <numeric>
+#include <stdexcept>
 #include <sstream>
 #include <string>
 #include <vector>
+
+std::string& TrimLeft(std::string& str)
+{
+    auto it2 = std::find_if(str.begin(), str.end(), [](char ch) {
+        return !std::isspace<char>(ch, std::locale::classic());
+    });
+    str.erase(str.begin(), it2);
+    return str;
+}
+
+std::string& TrimRight(std::string& str)
+{
+    auto it1 = std::find_if(str.rbegin(), str.rend(), [](char ch) {
+        return !std::isspace<char>(ch, std::locale::classic());
+    });
+    str.erase(it1.base(), str.end());
+    return str;
+}
+
+std::string& Trim(std::string& str) { return TrimLeft(TrimRight(str)); }
+
+std::string TrimAndCopy(std::string const& str)
+{
+    auto s = str;
+    return TrimLeft(TrimRight(s));
+}
 
 std::string JoinPath(const std::string& heading_path,
                      const std::string& tail_path) {
@@ -55,6 +89,37 @@ std::string ExtractFileName(const std::string& file_path) {
   return file_name;
 }
 
+bool FileExists(const std::string& name)
+{
+    std::ifstream ifs(name);
+    bool good = ifs.good();
+    if (!good)
+    {
+        std::cerr << name << " does not exists";
+    }
+    return good;
+}
+
+bool DirExists(const std::string& name)
+{
+    struct stat file_info;
+    if (stat(name.c_str(), &file_info) != 0)
+    {
+        return false;
+    }
+    return (file_info.st_mode & S_IFDIR) != 0;
+}
+
+bool CreateDirIfNotExists(const std::string& name)
+{
+    if (DirExists(name) == false)
+    {
+        int status = mkdir(name.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+        return status == 0;
+    }
+    return true;
+}
+
 std::vector<std::string> FileToLines(const std::string& file_path) {
   std::vector<std::string> lines;
   std::ifstream file(file_path.c_str(), std::ios::in);
@@ -66,6 +131,65 @@ std::vector<std::string> FileToLines(const std::string& file_path) {
   }
   return lines;
 }
+
+// TODO: this function sometimes return too early if the command has multiple response
+std::string Exec(const char* cmd)
+{
+    std::array<char, 128> buffer;
+    std::string result;
+    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+    if (!pipe)
+    {
+        throw std::runtime_error("popen() failed!");
+    }
+    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr)
+    {
+        result += buffer.data();
+    }
+    return result;
+}
+
+// return a current time as string
+// as format like 2020-01-23_01:23:45:678
+std::string GetCurrentTime()
+{
+    std::chrono::system_clock::time_point now =
+        std::chrono::system_clock::now();
+    std::chrono::system_clock::duration tp = now.time_since_epoch();
+
+    tp -= std::chrono::duration_cast<std::chrono::seconds>(tp);
+
+    time_t tt = std::chrono::system_clock::to_time_t(now);
+
+    tm t = *gmtime(&tt);
+
+    char buffer[50];
+    sprintf(buffer, "%04u-%02u-%02u_%02u:%02u:%02u:%03u", t.tm_year + 1900,
+            t.tm_mon + 1, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec,
+            static_cast<unsigned>(tp / std::chrono::milliseconds(1)));
+    std::string ret(buffer);
+    return ret;
+}
+
+// Timer for time measurement
+class Timer
+{
+public:
+    Timer() {}
+    inline void Start() { clock_gettime(CLOCK_MONOTONIC, &time1); }
+    inline double GetElapsed_ms() { return GetElapsed_ns() / 1000000.0; }
+    inline double GetElapsed_us() { return GetElapsed_ns() / 1000.0; }
+    inline double GetElapsed_ns()
+    {
+        clock_gettime(CLOCK_MONOTONIC, &time2);
+        return (
+            (1000000000.0 * static_cast<double>(time2.tv_sec - time1.tv_sec)) +
+            static_cast<double>(time2.tv_nsec - time1.tv_nsec));
+    }
+
+private:
+    struct timespec time1, time2;
+};
 
 template <typename Duration = std::chrono::microseconds, typename F,
           typename... Args>
