a515b495e1940e552f9e62f0cefa4c11548cb7f6
QianHao <qh.huang@eavisiontech.com>
2021-07-22 21:42:05 +0800

    add google foobar folder and challenges

---
+++ coding_practice/foobar_withgoogle/2_Lovely_Lucky_LAMBs/solution.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+def stingy_sol(total_lambs):
+    if total_lambs == 1:
+        return 1
+    
+
+def generous_sol(total_lambs):
+
+
+def solution(total_lambs):
+
+
+print(solution("code"))
